143b30ef724c601ba649eb8f451677cbe5412f45
Maner Niyet <manerniyet15@gmail.com>
2022-02-09 21:48:31 -0500

    add search func
    Search functionality and autocomplete functionality was added

--- src/App.js
+++ src/App.js
@@ -7,15 +7,35 @@ export class App extends Component {
   constructor() {
     super();
     this.state = {
-      
+      inputValue: "",
+      filteredBooks: []
     };
   }
 
+  handleFilter = (e) => {
+    const searchedWord = e.target.value;
+    this.setState({inputValue: searchedWord})
+    const newArray = data.filter(book => {
+      return book.title.toLowerCase().includes(searchedWord.toLowerCase())
+    })
+
+    if (searchedWord === "") {
+      this.setState({filteredBooks: []})
+    } else {
+      this.setState({filteredBooks: newArray})
+    }
+  }
+
+  handleClick = () => {
+    this.setState({inputValue: "", filteredBooks: []})
+  }
+
   render() {
+    const {inputValue, filteredBooks} = this.state;
     
     return (
       <div className="App">
-        <SearchBar data={data}/>
+        <SearchBar data={data} inputValue={inputValue} func={this.handleFilter} filteredBooks={filteredBooks} handleClick={this.handleClick}/>
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,39 @@
 import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import "./SearchBar.css";
-const SearchBar = ({ data }) => {
+import ClearIcon from '@mui/icons-material/Clear';
+const SearchBar = ({ inputValue, func, filteredBooks, handleClick }) => {
   return (
     <div className="search">
       <div className="searchInputs">
-        <input type="text" placeholder="Enter your book..." />
+        <input
+          type="text"
+          placeholder="Enter your book..."
+          onChange={func}
+          value={inputValue}
+        />
         <div className="searchIcon">
-          <SearchIcon />
+            {!inputValue ? (
+                 <SearchIcon />
+            ) : (
+                <ClearIcon onClick={handleClick}/>
+            )}
+         
         </div>
       </div>
-      <div className="dataResult">
-        {data.map((book) => {
-          return (
-            <a className="dataItem" href={book.link} target="_blank">
-              <p>{book.title}</p>
-            </a>
-          );
-        })}
-      </div>
+      {filteredBooks.length > 0 ? (
+        <div className="dataResult">
+          {filteredBooks.slice(0, 10).map((book) => {
+            return (
+              <a className="dataItem" href={book.link} target="_blank">
+                <p>{book.title}</p>
+              </a>
+            );
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
